ebeb5420ab319207588080b371c2ad5b1eefaa04
Akhil Narang <akhilnarang.1999@gmail.com>
2018-11-18 17:46:07 +0530

    sdm660-common: Move goodix firmware to /vendor/firmware
    Edit libgf_ca.so to look in /vendor/firmware instead of /system/etc/firmware as it only checked in
    /system/etc/firmware and /firmware/image
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    Signed-off-by: Subhajeet Muhuri <kenny3fcb@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -71,6 +71,10 @@ function blob_fixup() {
         sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "${2}"
         ;;
 
+    vendor/lib64/libgf_ca.so)
+        sed -i "s|/system/etc/firmware|/vendor/firmware\x0\x0\x0\x0|g" "${2}"
+        ;;
+
     vendor/lib/hw/camera.sdm660.so)
         patchelf --replace-needed libMiWatermark.so libMiWatermark_shim.so "${2}"
         patchelf --add-needed libcamera_sdm660_shim.so "${2}"
