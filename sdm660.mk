495cbd8c2efc67a8b491ff77e64103600de6d207
Volodymyr Zhdanov <wight554@gmail.com>
2019-05-08 06:57:28 +0000

    sdm660-common: set aapt perf config
    * bonito (p3axl) has same screen params and uses 400dpi
    * also update dpi prop to match aapt config
    
    Change-Id: Ife53ecc3b2ed5f981d23c2a6cba42aaa1d69fb90
    Signed-off-by: Anunay Maheshwari <anunaym14@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -37,6 +37,7 @@ $(call inherit-product, $(COMMON_PATH)/properties.mk)
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := 400dpi
 PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 
 # Boot animation
