e0fc58b2d9f0fafa4c334bf8c06485ceebdf5142
Volodymyr Zhdanov <wight554@gmail.com>
2019-05-08 06:54:51 +0000

    sdm660-common: set PRODUCT_AAPT_PREBUILT_DPI
    Signed-off-by: Anunay Maheshwari <anunaym14@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -37,7 +37,7 @@ $(call inherit-product, $(COMMON_PATH)/properties.mk)
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2160
